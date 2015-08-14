d53a6e145a2c456a7f2c3a0d474f4aca3115aaeb
Austen <rawleag@gmail.com>
2015-08-14 11:08:39 -0700

    added css, and game tiles shuffle

--- memoryGame.js
+++ memoryGame.js
@@ -1,32 +1,50 @@
-var cardPairsOne= ['dog', 'cat', 'cow', 'chicken', 'horse', 'rabbit', 'bird', 'ostrich'];
-var cardPairsTwo= ['dog', 'cat', 'cow', 'chicken', 'horse', 'rabbit', 'bird', 'ostrich'];
+var section = document.querySelectorAll('section')[0];
 
 
-function cardCreator (color) {
-		var newCard = document.createElement('div');
-		newCard.setAttribute('class', 'colorsToChooseFrom');
-		newCard.style.backgroundColor = color;
-		newCard.style.paddingBottom = '20px';
-		newCard.style.width = '20px';
+var cardPairsOne = ['dog', 'cat', 'cow', 'chicken', 'horse', 'rabbit', 'bird', 'ostrich', 'dog', 'cat', 'cow', 'chicken', 'horse', 'rabbit', 'bird', 'ostrich'];
+var shuffleCards = function (array) {
+  for (var i = 0; i < array.length * 100; i++){
+    var randomNum = Math.round(Math.random() * (array.length - 1));
+    var insert = array[randomNum];
+    array.splice(randomNum, 1);
+    array.unshift(insert);
+  }
+  return array;
+}
+var shuffledCards = shuffleCards(cardPairsOne);
+
+function cardCreator (card) {
+	var newCard = document.createElement('div');
+		newCard.setAttribute('class', shuffledCards[j]);
+		newCard.style.backgroundColor = 'blue';
+		newCard.style.paddingBottom = '200px';
+		newCard.style.width = '200px';
 		newCard.style.borderStyle = 'solid';
 		newCard.style.borderColor = '#c8c8c8';
 		newCard.style.borderWidth = '1px';
 		newCard.style.float = 'left';
 		newCard.addEventListener('click', function() {
-			colorPicked = color;
-			console.log(color);
+			
 		})
-		var list = document.querySelector('div');
-		list.appendChild(newCard);
+	section.appendChild(newCard);
+}
+
+	for(var j = 0; j < cardPairsOne.length; j++){
+		cardCreator(shuffledCards[j]);
 	}
 
-	for(var j = 0; j < colorArray.length; j++){
-		colorRow(colorArray[j]);
-	}
 
 
 	// All cards have the same background color to begin with.
 
 	// Add even listener, on click, show image of the word. 
 
-	// 
+	// //   shuffle: function (h){
+ //    for (var i = 0; i < h.length * 100; i++){
+ //      var randomNum = Math.round(Math.random() * (h.length - 1));
+ //      var insert = h[randomNum];
+ //      h.splice(randomNum, 1);
+ //      h.unshift(insert);
+ //    }
+ //    return h;
+ //  },
