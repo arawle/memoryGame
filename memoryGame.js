c02ffbbe0323432359babaa643c9b505cf2908ad
Austen <rawleag@gmail.com>
2015-08-17 07:49:54 -0700

    added timeout function

--- memoryGame.js
+++ memoryGame.js
@@ -3,6 +3,7 @@ var firstClass = null;
 var firstId = null;
 var pairCounter = 0;
 
+
 var cardPairsOne = ['dog', 'cat', 'cow', 'chicken', 'horse', 'rabbit', 'bird', 'ostrich', 'dog', 'cat', 'cow', 'chicken', 'horse', 'rabbit', 'bird', 'ostrich'];
 var shuffleCards = function (array) {
   for (var i = 0; i < array.length * 100; i++){
@@ -29,16 +30,28 @@ function cardCreator (cardId) {
         return firstClass = this.className;
       }
       else if (firstClass === newCard.className && firstId !== eventId) {
+        var firstElement = document.getElementById(firstId);
+        var secondElement = document.getElementById(eventId);
         newCard.innerHTML = newCard.className;
         firstClass = null;
         firstId = null;
         pairCounter += 1;
-        if (pairCounter === 8){
-          alert('You WIN!!!')
-        }
-        console.log('Correct');
-      }
-      else {
+          if (pairCounter === 8){
+            alert('You win!!!')
+          }
+          else {
+            alert(pairCounter)
+          };
+        firstElement.style.backgroundColor = 'inherit';
+        firstElement.style.borderWidth = '2px';
+        firstElement.style.borderColor = 'transparent';
+        firstElement.style.color = 'transparent';
+        secondElement.style.backgroundColor = 'inherit';
+        secondElement.style.borderWidth = '2px';
+        secondElement.style.borderColor = 'transparent';
+        secondElement.style.color = 'transparent';
+
+      } else {
         newCard.innerHTML = newCard.className;
         console.log('Try Again!');
         var firstElement = document.getElementById(firstId);
@@ -60,17 +73,16 @@ function cardCreator (cardId) {
 	for(var j = 0; j < cardPairsOne.length; j++){
 		cardCreator(j);
 	}
-  
+
 //add reset button event handler
 var button = document.querySelector('button');
 button.addEventListener('click', function(){
   location.reload('div');
-  console.log('hello');
 })
 
-
-
-
+ window.setTimeout(function(){ 
+   alert('Try Again!')
+   location.reload(window)}, 300000);
 
 
 
